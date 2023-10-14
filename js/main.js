27327ddbe44cd93b6db3c925a1dfaccb3ac3488f
cheo0405 <carlosalfredols@ufps.edu.co>
2023-10-13 21:54:08 -0500

    Muestra la lista de los cocteles con licor y sin alcohol

--- js/main.js
+++ js/main.js
@@ -1,9 +1,85 @@
-const cocteles = document.querySelector('.coctel-container')
+const namecoctel = document.getElementById('nameCoctel')
+const coctelImage = document.getElementById('coctelImage');
+const cocktailList = document.getElementById('coctelList');
 
-function fetchCoctel(){
-    fetch('www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
+function fetchCoctelSA() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
     .then(res => res.json())
-    .then(data => console.log(data))
+    .then(data => {
+        data.drinks.forEach(cocktail => {
+            
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.style.width = '18rem';
+
+            const image = document.createElement('img');
+            image.className = 'card-img-top';
+            image.src = cocktail.strDrinkThumb;
+            image.alt = cocktail.strDrink;
+
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            cardTitle.textContent = cocktail.strDrink;
+
+            const link = document.createElement('a');
+            link.href = 'agregarcocteles.html';
+            link.className = 'btn btn-primary';
+            link.textContent = 'Agregar al carrito';
+
+            card.appendChild(image);
+            card.appendChild(cardBody);
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(link);
+
+            coctelList.appendChild(card);
+            
+
+        });
+    })
+    .catch(error => console.error('Error:', error));
 }
 
-fetchCoctel();
+function fetchCoctelCA() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
+    .then(res => res.json())
+    .then(data => {
+        data.drinks.forEach(cocktail => {
+            
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.style.width = '18rem';
+
+            const image = document.createElement('img');
+            image.className = 'card-img-top';
+            image.src = cocktail.strDrinkThumb;
+            image.alt = cocktail.strDrink;
+
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            cardTitle.textContent = cocktail.strDrink;
+
+            const link = document.createElement('a');
+            link.href = 'agregarcocteles.html';
+            link.className = 'btn btn-primary';
+            link.textContent = 'Agregar al carrito';
+
+            card.appendChild(image);
+            card.appendChild(cardBody);
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(link);
+
+            coctelList.appendChild(card);
+            
+
+        });
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+fetchCoctelSA();
