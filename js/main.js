92da4e0b28cda3da6513aba1b14d1bccedaf9676
cheo0405 <carlosalfredols@ufps.edu.co>
2023-10-15 01:50:22 -0500

    YA agrega desde el inicio la opcion de los cocteles a la lista, falta oner el formualrio y al crrito de compra

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const parametro = urlParams.get('tipo');
+    const titulo = document.getElementById('titulo');
+
+    if (parametro === 'SA') {
+        titulo.textContent = 'Bebidas Sin Alcohol';
+        listadoBebidasSinAlcohol(); 
+    } else if (parametro === 'CA') {
+        titulo.textContent = 'Bebidas Con Alcohol';
+        listadoBebidasConAlcohol(); 
+
+    } else if(parametro === 'trago'){
+        titulo.textContent = 'Bebidas Ordinarias';
+        listadoBebidasOrdinarias();
+
+    } else if(parametro === 'coctel'){
+        titulo.textContent = 'Cocteles';
+        listadoCocteles();
+
+    }
+});
+
 const namecoctel = document.getElementById('nameCoctel')
 const coctelImage = document.getElementById('coctelImage');
 const cocktailList = document.getElementById('coctelList');
+const precio = document.getElementById('precio');
 
-function fetchCoctelSA() {
+function listadoBebidasSinAlcohol() {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Non_Alcoholic')
     .then(res => res.json())
     .then(data => {
@@ -24,6 +48,9 @@ function fetchCoctelSA() {
             cardTitle.className = 'card-title';
             cardTitle.textContent = cocktail.strDrink;
 
+            const p = document.createElement('p');
+            p.textContent = 'Precio: ' + cocktail.idDrink; 
+
             const link = document.createElement('a');
             link.href = 'agregarcocteles.html';
             link.className = 'btn btn-primary';
@@ -32,17 +59,17 @@ function fetchCoctelSA() {
             card.appendChild(image);
             card.appendChild(cardBody);
             cardBody.appendChild(cardTitle);
+            cardBody.appendChild(p);
             cardBody.appendChild(link);
 
-            coctelList.appendChild(card);
+            cocktailList.appendChild(card);
             
-
         });
     })
     .catch(error => console.error('Error:', error));
 }
 
-function fetchCoctelCA() {
+function listadoBebidasConAlcohol() {
     fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?a=Alcoholic')
     .then(res => res.json())
     .then(data => {
@@ -64,6 +91,9 @@ function fetchCoctelCA() {
             cardTitle.className = 'card-title';
             cardTitle.textContent = cocktail.strDrink;
 
+            const p = document.createElement('p');
+            p.textContent = 'Precio: ' + cocktail.idDrink; 
+
             const link = document.createElement('a');
             link.href = 'agregarcocteles.html';
             link.className = 'btn btn-primary';
@@ -72,9 +102,10 @@ function fetchCoctelCA() {
             card.appendChild(image);
             card.appendChild(cardBody);
             cardBody.appendChild(cardTitle);
+            cardBody.appendChild(p);
             cardBody.appendChild(link);
 
-            coctelList.appendChild(card);
+            cocktailList.appendChild(card);
             
 
         });
@@ -82,4 +113,88 @@ function fetchCoctelCA() {
     .catch(error => console.error('Error:', error));
 }
 
-fetchCoctelSA();
+function listadoBebidasOrdinarias() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Ordinary_Drink')
+    .then(res => res.json())
+    .then(data => {
+        data.drinks.forEach(cocktail => {
+            
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.style.width = '18rem';
+
+            const image = document.createElement('img');
+            image.className = 'card-img-top';
+            image.src = cocktail.strDrinkThumb;
+            image.alt = cocktail.strDrink;
+
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            cardTitle.textContent = cocktail.strDrink;
+
+            const p = document.createElement('p');
+            p.textContent = 'Precio: ' + cocktail.idDrink; 
+
+            const link = document.createElement('a');
+            link.href = 'agregarcocteles.html';
+            link.className = 'btn btn-primary';
+            link.textContent = 'Agregar al carrito';
+
+            card.appendChild(image);
+            card.appendChild(cardBody);
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(p);
+            cardBody.appendChild(link);
+
+            cocktailList.appendChild(card);
+            
+        });
+    })
+    .catch(error => console.error('Error:', error));
+}
+
+function listadoCocteles() {
+    fetch('https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
+    .then(res => res.json())
+    .then(data => {
+        data.drinks.forEach(cocktail => {
+            
+            const card = document.createElement('div');
+            card.className = 'card';
+            card.style.width = '18rem';
+
+            const image = document.createElement('img');
+            image.className = 'card-img-top';
+            image.src = cocktail.strDrinkThumb;
+            image.alt = cocktail.strDrink;
+
+            const cardBody = document.createElement('div');
+            cardBody.className = 'card-body';
+
+            const cardTitle = document.createElement('h5');
+            cardTitle.className = 'card-title';
+            cardTitle.textContent = cocktail.strDrink;
+
+            const p = document.createElement('p');
+            p.textContent = 'Precio: ' + cocktail.idDrink; 
+
+            const link = document.createElement('a');
+            link.href = 'agregarcocteles.html';
+            link.className = 'btn btn-primary';
+            link.textContent = 'Agregar al carrito';
+
+            card.appendChild(image);
+            card.appendChild(cardBody);
+            cardBody.appendChild(cardTitle);
+            cardBody.appendChild(p);
+            cardBody.appendChild(link);
+
+            cocktailList.appendChild(card);
+            
+        });
+    })
+    .catch(error => console.error('Error:', error));
+}
